433320f86bc94a93e8fdeff1b506d70923ecbd67
Andero Piir <andero.piir@mail.ee>
2019-05-08 13:24:21 +0300

    Backup

---
+++ praks4a/yl3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#
+tar -czf /home/user/skriptimine/praks4a/backup/test.tar.gz
+echo "kataloogi /home/user/skriptimine/praks4a/test backupi nimi test.tar.gz & asub /home/user/skriptimine/praks4a/backup kaustas"
+#
+#Skripti lõpp
+
